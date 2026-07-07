6662807a34a9cf2804063e587a2ec4f6ce974d50
Marjan-maxa <boktimd95@gmail.com>
2026-07-07 14:02:40 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
